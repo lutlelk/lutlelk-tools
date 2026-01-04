e873ae6f44540e4bbe1ebe23838169fe2056ec3c
lutl <lutlelk@gmail.com>
2026-01-04 19:56:36 +0800

    第三次提交

--- test-single-import.mjs
+++
@@ -1,9 +0,0 @@
-import { chunk } from './packages/array/dist/chunk.js'
-import { debounce } from './packages/function/dist/debounce.js'
-import { $, $$ } from './packages/dom/dist/selector.js'
-
-console.log('Testing single-file imports:')
-console.log('chunk([1, 2, 3, 4, 5], 2):', chunk([1, 2, 3, 4, 5], 2))
-console.log('debounce function:', debounce(() => console.log('test'), 100))
-console.log('$ selector:', $)
-console.log('$$ selector:', $$)
